6c14fa720cbf516d7ba9cd37d67497c55ba90276
Hyyan Abo Fakher <hyyanaf@gmail.com>
2018-08-16 15:31:25 +0200

    intial commit

--- web/pdfjs/web/api.js
+++
@@ -1,7 +0,0 @@
-function myFooFunction() {
-
-    console.log(window.PDFViewerApplication);
-    console.log($wnd.PDFViewerApplication);
-    console.log(window.top.PDFViewerApplication);
-    console.log($wnd.top.PDFViewerApplication);
-}
